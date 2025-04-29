b8d7ed772b1c4fa9be0c424e2f038a258face2c7
TheoKevH <theodorekevinh@gmail.com>
2025-04-29 11:18:29 +0700

    feat: create role based registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'), 
     path('profile/', views.profile_view, name='profile'),
     path('change-password/', views.change_password_view, name='change_password'),
+    path('role/', views.role_selection, name='role'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -361,3 +361,6 @@ def change_password_view(request):
     }
     
     return render(request, 'authentication/change_password.html', context)
+
+def role_selection(request):
+    return render(request, 'authentication/role.html')
